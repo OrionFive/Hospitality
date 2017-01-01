577072b4b192ce876c41be62bde9db2e5a55673d
Rando Wiltschek <randomw@gmx.net>
2017-01-01 16:41:38 +0100

    Fixed CanIngestForJoy method

--- Source/Source/Detouring/JoyGiver_Ingest.cs
+++ Source/Source/Detouring/JoyGiver_Ingest.cs
@@ -9,15 +9,12 @@ namespace Hospitality.Detouring
     internal static class JoyGiver_Ingest
     {
         [Detour(typeof(Source), bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance)]
-        public static bool CanUseIngestItemForJoy(Pawn pawn, Thing t)
+        public static bool CanIngestForJoy(Pawn pawn, Thing t)
         {
-            if (!t.def.IsIngestible) return false; // Added to prevent error (t.def is human... wtf RimWorld)
-
-            if (t.def.ingestible.joyKind == null || t.def.ingestible.joy <= 0f)
+            if (!t.def.IsIngestible || t.def.ingestible.joyKind == null || t.def.ingestible.joy <= 0f)
             {
                 return false;
             }
-
             if (t.Spawned)
             {
                 if (!pawn.CanReserve(t, 1))
@@ -34,8 +31,7 @@ namespace Hospitality.Detouring
                 }
             }
             //Log.Message(t.def.IsDrug + ": " + (pawn.drugs == null));
-            if (t.def.IsDrug && pawn.drugs != null && !pawn.drugs.CurrentPolicy[t.def].allowedForJoy
-                && pawn.story != null)
+            if (t.def.IsDrug && pawn.drugs != null && !pawn.drugs.CurrentPolicy[t.def].allowedForJoy && pawn.story != null)
             {
                 int num = pawn.story.traits.DegreeOfTrait(TraitDefOf.DrugDesire);
                 if (num <= 0 && !pawn.InMentalState)
