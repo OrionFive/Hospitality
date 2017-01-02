f76a9f0fc1d8aefe3ba3164afae43af654228ff5
Rando Wiltschek <randomw@gmx.net>
2017-01-02 12:39:29 +0100

    Attempt at fixing "item is reserved" error when browsing

--- Source/Source/JobDriver_BrowseItems.cs
+++ Source/Source/JobDriver_BrowseItems.cs
@@ -11,7 +11,7 @@ namespace Hospitality
             //this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
             //this.FailOn(() => JoyGiver_BuyStuff.IsBuyableNow(pawn, TargetThingA));
             foreach (var toil in base.MakeNewToils()) yield return toil;
-
+            yield return Toils_Reserve.Release(TargetIndex.A);
         }
     }
 }
