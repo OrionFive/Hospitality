9f28404d695a05a8c0245c677904b5226abb7e3e
Rando Wiltschek <randomw@gmx.net>
2017-01-15 17:02:11 +0100

    Added debug log for breaking pawns

--- Source/Source/EventAction_BreakPawns.cs
+++ Source/Source/EventAction_BreakPawns.cs
@@ -1,13 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using Verse;
+using Verse.AI.Group;
 
 namespace Hospitality
 {
     public class EventAction_BreakPawns : EventAction_Pawns
     {
-        public EventAction_BreakPawns(List<Pawn> pawns)
+        public EventAction_BreakPawns(List<Pawn> pawns, PawnLostCondition condition)
         {
+            Log.Message("Break reason: "+condition);
             this.pawns = pawns;
         }
         
