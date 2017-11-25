0144ce5d93ec1505edd14f0b90deec65f4552df8
Rando Wiltschek <randomw@gmx.net>
2017-11-25 20:35:55 +0100

    Fixed that player can select guest area from inspect view

--- Source/Source/GuestUtility.cs
+++ Source/Source/GuestUtility.cs
@@ -427,6 +427,7 @@ namespace Hospitality
             guest.MapHeld.mapPawns.UpdateRegistryForPawn(guest);
 
             guest.playerSettings.medCare = MedicalCareCategory.Best;
+            guest.playerSettings.AreaRestriction = null;
 
             if (guest.caller != null) guest.caller.DoCall();
         }
