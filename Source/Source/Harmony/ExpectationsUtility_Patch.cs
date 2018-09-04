407d9a77fdc54f2d949d1ec95072e5c95e21bf84
Rando Wiltschek <randomw@gmx.net>
2018-09-04 16:56:54 +0200

    - Fix: Hospitality didn't do expectations the B19 way

---
+++ Source/Source/Harmony/ExpectationsUtility_Patch.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace Hospitality.Harmony
+{
+    /// <summary>
+    /// Different expectations for guests (and expectations at all!)
+    /// </summary>
+    public class ExpectationsUtility_Patch
+    {
+        [HarmonyPatch(typeof(ExpectationsUtility), "CurrentExpectationFor", new[] { typeof(Pawn) })]
+        public class CurrentExpectationForPawn
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ref ExpectationDef __result, Pawn p)
+            {
+                if (__result == null) return; // Original method aborted, so will we
+                if (p.IsGuest())
+                {
+                    __result = CurrentExpectationFor(p.MapHeld);
+                }
+            }
+
+            // Copied
+            private static ExpectationDef CurrentExpectationFor(Map m)
+            {
+                float wealthTotal = m.wealthWatcher.WealthTotal * 2; // Doubled for guests
+                var list = Traverse.Create(typeof(ExpectationsUtility)).Field("expectationsInOrder").GetValue<List<ExpectationDef>>(); // had to add
+                foreach (ExpectationDef expectationDef in list) 
+                {
+                    if (wealthTotal < expectationDef.maxMapWealth)
+                    {
+                        return expectationDef;
+                    }
+                }
+                return list[list.Count - 1];
+            }
+        }
+    }
+}
