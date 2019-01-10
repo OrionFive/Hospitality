7dbec9c1d9ceae053f5469febedcc589e646e028
Rando Wiltschek <randomw@gmx.net>
2019-01-10 20:02:46 +0100

    Added check for factions without peaceful pawngroupmakers

--- Source/Source/DefsUtility.cs
+++ Source/Source/DefsUtility.cs
@@ -13,17 +13,33 @@ namespace Hospitality
         public static void CheckForInvalidDefs()
         {
             CheckChemicalDefs();
+            CheckFactionDefs();
+        }
+
+        // Must have at least one pawn group maker of type "Peaceful", if ever non hostile
+        private static void CheckFactionDefs()
+        {
+            foreach (var factionDef in DefDatabase<FactionDef>.AllDefsListForReading.Where(f=>!f.isPlayer && !f.hidden && f.CanEverBeNonHostile))
+            {
+                if (factionDef.pawnGroupMakers == null || !factionDef.pawnGroupMakers.Any(pgm => pgm.kindDef.defName == "Peaceful"))
+                {
+                    LogMisconfiguration(factionDef, $"FactionDef {factionDef.defName} must have at least one pawnGroupMaker with kindDef 'Peaceful', or 'permanentEnemy', 'isPlayer' or 'hidden' must be set to true.");
+                }
+            }
         }
 
         private static void CheckChemicalDefs()
         {
-            var invalidDefs = DefDatabase<ChemicalDef>.AllDefsListForReading.Where(x => x.addictionHediff == null);
-
-            foreach (var def in invalidDefs)
+            foreach (var def in DefDatabase<ChemicalDef>.AllDefsListForReading.Where(x => x.addictionHediff == null))
             {
-                Log.ErrorOnce($"The ChemicalDef {def.defName} has no addictionHediff. Remove the ChemicalDef or add an addiction hediff. " 
-                              + $"Otherwise this will cause random groups and raids to not spawn.", def.shortHash + 83747646);
+                LogMisconfiguration(def, $"The ChemicalDef {def.defName} has no addictionHediff. Remove the ChemicalDef or add an addiction hediff. Otherwise this will cause random groups and raids to not spawn.");
             }
+        }
+
+        private static void LogMisconfiguration(Def def, string message)
+        {
+            var commaList = LoadedModManager.RunningModsListForReading.Where(m => m.AllDefs.Contains(def)).Select(m => m.Name).ToCommaList(true);
+            Log.ErrorOnce($"{message} This is a misconfiguration in {commaList}.", def.shortHash + 83747646);
         }
     }
 }
