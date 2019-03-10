7b7b94bc589797e3bdcbe9ebfe9ec85e7c7a33d4
Rando Wiltschek <randomw@gmx.net>
2019-03-10 15:36:00 +0100

    - New: Added "..." button in guests tab that opens the personal guest tab

---
+++ Source/Source/MainTab/PawnColumnWorker_ButtonMore.cs
@@ -0,0 +1,32 @@
+using System;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace Hospitality.MainTab {
+    public class PawnColumnWorker_ButtonMore : PawnColumnWorker
+    {
+        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
+        {
+            if (Widgets.ButtonText(rect, "..."))
+            {
+                CameraJumper.TryJumpAndSelect(pawn);
+                if (Find.Selector.IsSelected(pawn))
+                {
+                    InspectPaneUtility.OpenTab(typeof (ITab_Pawn_Guest));
+                }
+            }
+        }
+
+        public override int GetMinWidth(PawnTable table)
+        {
+            return Mathf.Max(base.GetMinWidth(table), 36);
+        }
+
+        public override int GetMaxWidth(PawnTable table)
+        {
+            return Mathf.Min(base.GetMaxWidth(table), this.GetMinWidth(table));
+        }
+    }
+}
