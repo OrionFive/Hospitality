6aa368ad5cd7b3e3d21d9ad3d5d342381a166421
Rando Wiltschek <randomw@gmx.net>
2020-03-17 19:23:07 +0100

    Fixed issue with guests standing around outside base (hopefully)

--- Source/Source/JobGiver_GotoGuestArea.cs
+++ Source/Source/JobGiver_GotoGuestArea.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -12,7 +13,8 @@ namespace Hospitality
             if (area == null) return 0;
             if (area.TrueCount == 0) return 0;
 
-            return area[pawn.PositionHeld] ? 0 : 10;
+            var result = area[pawn.PositionHeld] ? 0 : 10;
+            return result;
         }
 
         public override ThinkResult TryIssueJobPackage(Pawn pawn, JobIssueParams jobParams)
@@ -21,10 +23,21 @@ namespace Hospitality
             if (area == null) return ThinkResult.NoJob;
             if (area.TrueCount == 0) return ThinkResult.NoJob;
 
+            // Find nearby
             bool found = CellFinder.TryFindRandomReachableCellNear(pawn.Position, pawn.MapHeld, 20, 
                 TraverseParms.For(pawn, Danger.Some, TraverseMode.PassDoors), c => area[c], null, out var closeSpot);
 
-            return found ? new ThinkResult(new Job(JobDefOf.Goto, closeSpot) {locomotionUrgency = LocomotionUrgency.Jog}, this) : ThinkResult.NoJob;
+            if (!found)
+            {
+                // Find any
+                closeSpot = area.ActiveCells.InRandomOrder().Where(cell => area.Map.pathGrid.WalkableFast(cell)).Take(20)
+                    .FirstOrDefault(cell => pawn.CanReach(cell, PathEndMode.OnCell, Danger.Some, false, TraverseMode.PassDoors));
+                found = closeSpot.IsValid;
+            }
+
+            var job = found ? new ThinkResult(new Job(JobDefOf.Goto, closeSpot) {locomotionUrgency = LocomotionUrgency.Jog}, this) : ThinkResult.NoJob;
+
+            return job;
         }
     }
 }
