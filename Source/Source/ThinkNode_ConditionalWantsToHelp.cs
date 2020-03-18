7fe7e87ce5de54d1c4de554eac10c84e2340ae58
Rando Wiltschek <randomw@gmx.net>
2020-03-18 17:29:03 +0100

    Made required happiness for helping xml tweakable

--- Source/Source/ThinkNode_ConditionalWantsToHelp.cs
+++ Source/Source/ThinkNode_ConditionalWantsToHelp.cs
@@ -5,12 +5,13 @@ namespace Hospitality
 {
     public class ThinkNode_ConditionalWantsToHelp : ThinkNode_Conditional
     {
+        public float requiredHappiness = 0.65f;
         protected override bool Satisfied(Pawn pawn)
         {
             if (Settings.disableWork) return false;
             if (pawn.needs?.mood == null) return false;
-            var isHappy = pawn.needs.mood.CurLevel > 0.9f;
-            if(isHappy) Log.Message($"{pawn.LabelShort} wants to help");
+            var isHappy = pawn.needs.mood.CurLevel > requiredHappiness;
+            // See JobGiver_Work_Patch for more complex formula
             return isHappy;
         }
     }
