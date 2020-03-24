da42b26f8c640850b27693aa2703786c6796e67e
Rando Wiltschek <randomw@gmx.net>
2020-03-24 18:06:31 +0100

    - Fix: Guests won't help with feeding prisoners or sick; too many issues (food from inventory, wrong food category, etc. etc.)

--- Source/Source/Harmony/JobGiver_Work_Patch.cs
+++ Source/Source/Harmony/JobGiver_Work_Patch.cs
@@ -26,6 +26,7 @@ namespace Hospitality.Harmony
                 if (Settings.disableArtAndCraft.Value && IsArtOrCraft(giver.def.workType.workTags)) return false;
                 if (Settings.disableOperations.Value && IsOperation(giver)) return false;
                 if (Settings.disableMedical.Value && IsOperation(giver) || IsMedical(giver)) return false;
+                if (IsFeeding(giver)) return false; // Too many problems with this (uses food from inventory, wrong food category, etc.
 
                 if (!pawn.GetVisitScore(out var score)) return false;
 
@@ -63,6 +64,10 @@ namespace Hospitality.Harmony
             private static bool IsMedical(WorkGiver workGiver)
             {
                 return workGiver is WorkGiver_Tend;
+            }
+            private static bool IsFeeding(WorkGiver workGiver)
+            {
+                return workGiver is WorkGiver_FeedPatient || workGiver is WorkGiver_Warden_Feed;
             }
 
             private static bool IsSkilledEnough(Pawn pawn, WorkTypeDef workTypeDef)
