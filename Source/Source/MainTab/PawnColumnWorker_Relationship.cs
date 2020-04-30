10711a8bc67387063757d2118b01ceb509171a3e
Rando Wiltschek <randomw@gmx.net>
2020-04-30 14:39:40 +0200

    - New: When unsorted, the guest tab now shows which guests are in the same group

--- Source/Source/MainTab/PawnColumnWorker_Relationship.cs
+++ Source/Source/MainTab/PawnColumnWorker_Relationship.cs
@@ -12,6 +12,8 @@ namespace Hospitality.MainTab
     {
         private static readonly Texture2D Icon = ContentFinder<Texture2D>.Get("UI/Tab/Relationship");
         private static Dictionary<int, RelationInfo> relationCache = new Dictionary<int, RelationInfo>();
+        private static int lastFrame;
+        private bool mayDrawLordGroups;
 
         private class RelationInfo
         {
@@ -28,6 +30,7 @@ namespace Hospitality.MainTab
 
         protected override Texture2D GetIconFor(Pawn pawn)
         {
+            if (pawn == null) return null;
             return GetRelationInfo(pawn).hasRelationship ? Icon : null;
         }
 
@@ -72,6 +75,23 @@ namespace Hospitality.MainTab
         protected override string GetIconTip(Pawn pawn)
         {
             return GetRelationInfo(pawn).tooltip;
+        }
+
+        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
+        {
+            base.DoCell(rect, pawn, table);
+
+            // Only run once
+            if (!mayDrawLordGroups) return;
+            mayDrawLordGroups = false;
+
+            MainTabUtility.DrawLordGroups(rect, table, pawn);
+        }
+
+        public override void DoHeader(Rect rect, PawnTable table)
+        {
+            base.DoHeader(rect, table);
+            mayDrawLordGroups = true;
         }
     }
 }
