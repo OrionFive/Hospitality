6ee631d0c0b9b9f4db51adc9fe120561df98b226
simplyWiri <48577820+simplyWiri@users.noreply.github.com>
2020-10-27 14:49:29 +1100

    Optimize ThingFilter patch Use local path to most recent (rw) harmony instead of packages

--- Source/Source/Harmony/ThingFilter_Patch.cs
+++ Source/Source/Harmony/ThingFilter_Patch.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
 using Verse;
 
 namespace Hospitality.Harmony
@@ -11,16 +13,23 @@ namespace Hospitality.Harmony
         [HarmonyPatch(typeof(ThingFilter), nameof(ThingFilter.Allows), typeof(ThingDef))]
         public class Allows
         {
+            private static Dictionary<string, ThingDef> bedNameToThing = new Dictionary<string, ThingDef>();
             [HarmonyPrefix]
             public static bool Prefix(ref ThingDef def)
             {
                 // Is from a guest bed?
-                if (def?.defName != null && def.thingClass == typeof(Building_GuestBed))
+                if (def?.defName == null || def.thingClass != typeof(Building_GuestBed)) return true;
+
+
+                if (!bedNameToThing.TryGetValue(def.defName, out var bedDef))
                 {
-                    var bedDef = DefDatabase<ThingDef>.GetNamed(def.defName.Substring(0, def.defName.Length - 5)); // remove "Guest" from name
-                    // Use def of original bed instead
-                    if (bedDef != null) def = bedDef;
+                    bedDef = DefDatabase<ThingDef>.GetNamed(def.defName.Substring(0, def.defName.Length - 5)); // remove "Guest" from name
+                    bedNameToThing.Add(def.defName, bedDef); // we don't care if its null, just trying to avoid the string manipulation and DefDatabase lookups
                 }
+
+                // Use def of original bed instead
+                if (bedDef != null)
+                    def = bedDef;
 
                 // Business as usual
                 return true;
