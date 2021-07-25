9b59a8a5db61f54d6c0eb8e158a9903cf9363578
OrionFive <wiltschek@gmx.net>
2021-07-25 17:19:16 +0200

    Made food restrictions save correctly

--- Source/Source/Hospitality_GameComponent.cs
+++ Source/Source/Hospitality_GameComponent.cs
@@ -11,29 +11,23 @@ namespace Hospitality
 		public Hospitality_GameComponent(Game game)
 		{
 			// Bug: Why does this run 3 times when loading a game?
-			defaultFoodRestriction ??= CreateDefaultFoodRestriction();
+			defaultFoodRestriction ??= new FoodRestriction(600, "Hospitality_Guests"); // Arbitrary ID
+			ApplyFoodFilters(defaultFoodRestriction);
 		}
 
-		//public override void ExposeData()
-		//{
-		//	Scribe_Deep.Look(ref defaultFoodRestriction, "defaultFoodRestriction");
-		//}
-
-		public static FoodRestriction CreateDefaultFoodRestriction()
+		public override void ExposeData()
 		{
-			const int id = 600; // Arbitrary number
-			FoodRestriction foodRestriction = new FoodRestriction(id, "Hospitality_Guests");
+			Scribe_Deep.Look(ref defaultFoodRestriction, "defaultFoodRestriction");
+		}
+
+		private static void ApplyFoodFilters(FoodRestriction foodRestriction)
+		{
 			foodRestriction.filter.SetAllow(ThingCategoryDefOf.Foods, true);
 			foodRestriction.filter.SetAllow(ThingCategoryDefOf.Drugs, true);
+			//foodRestriction.filter.SetAllow(ThingCategoryDefOf.MeatRaw, false);
 			foodRestriction.filter.SetAllow(ThingCategoryDefOf.CorpsesHumanlike, false);
 			foodRestriction.filter.SetAllow(ThingCategoryDefOf.CorpsesAnimal, false);
-			//foodRestriction.filter.SetAllow(ThingCategoryDefOf.MeatRaw, false);
-
 			//Log.Message($"Guest food restriction: {foodRestriction.filter.allowedDefs.Where(d=>foodRestriction.Allows(d)).Select(d=>d.label).ToCommaList()}");
-
-			return foodRestriction;
 		}
-
-
 	}
 }
