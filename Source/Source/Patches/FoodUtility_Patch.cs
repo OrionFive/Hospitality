bafd6c75f889df054f868343c674489d72045271
OrionFive <wiltschek@gmx.net>
2021-08-02 11:36:14 +0200

    Renamed namespace Harmony to Patches

---
+++ Source/Source/Patches/FoodUtility_Patch.cs
@@ -0,0 +1,25 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Hospitality.Patches
+{
+    internal static class FoodUtility_Patch
+    {
+        /// <summary>
+        /// So guests will care
+        /// </summary>
+        [HarmonyPatch(typeof(FoodUtility), nameof(FoodUtility.BestFoodSourceOnMap))]
+        public class BestFoodSourceOnMap
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(Pawn eater, bool desperate)
+            {
+                if (!eater.IsArrivedGuest(out _)) return true;
+                return desperate;
+            }
+        }
+    }
+}
+
+    
---
+++ Source/Source/Patches/Pawn_GuestTracker_Patch.cs
@@ -0,0 +1,23 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Hospitality.Patches
+{
+    /// <summary>
+    /// Detoured so guests don't become prisoners
+    /// </summary>
+    internal static class Pawn_GuestTracker_Patch
+    {
+        [HarmonyPatch(typeof(Pawn_GuestTracker), nameof(Pawn_GuestTracker.SetGuestStatus))]
+        public class SetGuestStatus
+        {
+            [HarmonyPrefix]
+            public static void Prefix(ref GuestStatus guestStatus, Pawn ___pawn)
+            {
+                // Added
+                if (___pawn?.IsGuest() == true) guestStatus = GuestStatus.Guest;
+            }
+        }
+    }
+}
