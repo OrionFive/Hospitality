feb80645130b28ff809492ca6367eb12c51805e7
Rando Wiltschek <wiltschek@gmx.net>
2022-01-17 10:36:38 +0100

    - Fix #660: Minimum group size resetting to 1 if above 16

--- Source/Source/Settings.cs
+++ Source/Source/Settings.cs
@@ -84,7 +84,7 @@ namespace Hospitality
         // Make sure that it still works when referenced settings are null!
         private static SettingHandle.ValueIsValid WorkSkillLimits { get { return AtLeast(() => disableLimits?.Value != false ? 0 : 6); } }
         private static SettingHandle.ValueIsValid MaxIncidentsPer3DaysLimitsMin { get { return AtLeast(() => 1); } }
-        private static SettingHandle.ValueIsValid GroupSizeLimitsMin { get { return Between(() => 1, () => maxGuestGroupSize?.Value ?? DefaultMaxGroupSize); } }
+        private static SettingHandle.ValueIsValid GroupSizeLimitsMin { get { return Between(() => 1, () => maxGuestGroupSize?.Value ?? int.MaxValue); } }
         private static SettingHandle.ValueIsValid GroupSizeLimitsMax { get { return AtLeast(() => Mathf.Max(minGuestGroupSize?.Value ?? 0, disableLimits?.Value != false ? 1 : 8)); } }
 
         private static SettingHandle.ValueIsValid Between(Func<int> amountMin, Func<int> amountMax)
