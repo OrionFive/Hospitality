6b6769769e8ed556d18f4a67606e1a1eb5d4fb30
Rando Wiltschek <rando@extra-nice.net>
2025-07-29 19:14:04 +0200

    Build

--- Source/Source/Patches/Pawn_MindState_Patch.cs
+++ Source/Source/Patches/Pawn_MindState_Patch.cs
@@ -2,23 +2,22 @@ using HarmonyLib;
 using Hospitality.Utilities;
 using Verse.AI;
 
-namespace Hospitality.Patches
-{
-    /// <summary>
-    /// So downed guests stay traders.
-    /// </summary>
-    public class Pawn_MindState_Patch
-    {
-        [HarmonyPatch(typeof(Pawn_MindState), nameof(Pawn_MindState.Reset), new Type[]{ typeof(bool), typeof(bool), typeof(bool) })]
-        public class TryStartMentalState
-        {
-            [HarmonyPostfix]
-            public static void Postfix(ref Pawn_MindState __instance)
-            {
-                if (!__instance.pawn.IsGuest()) return;
+namespace Hospitality.Patches;
 
-                __instance.pawn.ConvertToTrader(false);
-            }
+/// <summary>
+///     So downed guests stay traders.
+/// </summary>
+public class Pawn_MindState_Patch
+{
+    [HarmonyPatch(typeof(Pawn_MindState), nameof(Pawn_MindState.Reset), typeof(bool), typeof(bool), typeof(bool))]
+    public class TryStartMentalState
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref Pawn_MindState __instance)
+        {
+            if (!__instance.pawn.IsGuest()) return;
+
+            __instance.pawn.ConvertToTrader(false);
         }
     }
 }
